b85d6d66b4301e9f4579e7b68ba33a21d68f7076
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-06-15 17:01:22 +0200

    Result of the game

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ if(randomNumber == 1){
 }
 printMessage('Mój ruch to: ' + computerMove);
 
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -32,4 +33,26 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Ty przegrywasz!');
+}else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Ty przegrywasz!');
+}else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Ty przegrywasz!');
+}else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Jest remis!');
+}else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Jest remis!');
+}else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Jest remis!');   
+}else if (playerMove != '1, 2, 3') {
+    printMessage('Nieznany ruch!');
+}
 
