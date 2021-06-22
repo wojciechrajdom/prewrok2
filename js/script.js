f9752006305f4b15795eb322f037a635098419cb
Wojciech Glowa <wojciechrajdom@gmail.com>
2021-06-22 11:37:55 +0200

    Gra poprawiona

--- js/script.js
+++ js/script.js
@@ -19,34 +19,8 @@ function playGame(playerInput = '1' + '2' + '3'){
     console.log('Wylosowana liczba to: ' + randomNumber);
 
     let computerMove = getMoveName(randomNumber);
-
-    /*if(randomNumber == 1){
-    computerMove = 'kamień';
-
-    }else if (randomNumber == 2) {
-        computerMove = 'papier';
-
-    }else if (randomNumber == 3) {
-        computerMove = 'nożyce';
-    }*/
+    
     printMessage('Zagrałem: ' + computerMove);
-
-
-    /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-    console.log('Gracz wpisał: ' + playerInput);
-
-    let playerMove = getMoveName(playerInput);
-
-    /*if(playerInput == '1'){
-    playerMove = 'kamień';
-
-    }else if (playerInput == '2') {
-        playerMove = 'papier';
-        
-    }else if (playerInput == '3') {
-        playerMove = 'nożyce';
-    }*/
 
     printMessage('Ty: ' + playerMove);
 
@@ -79,4 +53,4 @@ document.getElementById(id='play-paper').addEventListener('click', function(){
 }); 
 document.getElementById(id='play-scissors').addEventListener('click', function(){
     playGame(3);
-});   
+});  
